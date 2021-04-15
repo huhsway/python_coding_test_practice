e87d4a6e5ed41af644085ad605ea92807ac8787f
Huhsway <seunghoe9542@naver.com>
2021-04-15 18:51:46 +0900

    오타 수정

--- CHAPTER 05 DFS BFS/5-11.py
+++ CHAPTER 05 DFS BFS/5-11.py
@@ -15,7 +15,7 @@ dy = [0,0,-1,1]
 def bfs(x, y):
     # 큐(Queue) 구현을 위해 deque 라이브러리 사용
     queue = deque()
-    queue.appnd((x,y))
+    queue.append((x,y))
     # 큐가 빌 때까지 반복
     while queue:
         x, y = queue.popleft()
