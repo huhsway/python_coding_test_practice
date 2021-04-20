cc123f4c957b8de41b81a33ed45862282af4d2b1
Huhsway <seunghoe9542@naver.com>
2021-04-20 15:48:45 +0900

    코드 수정

--- CHAPTER 10 그래프 이론/10-8.py
+++ CHAPTER 10 그래프 이론/10-8.py
@@ -18,10 +18,6 @@ def union_parent(parent, a, b):
 v, e = map(int,input().split())
 parent = [0] * (v+1) # 부모 테이블 초기화
 
-# 노드의 개수와 간선(union 연산)의 개수 입력받기
-v, e = map(int,input().split())
-parent = [0] * (v+1) # 부모 테이블 초기화
-
 # 모든 간선을 담을 리스트와 최종 비용을 담을 변수
 edges = []
 result = 0
