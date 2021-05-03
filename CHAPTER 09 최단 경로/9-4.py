c6e24f233d8aff965f395e7b3e696e0aaf1ded13
Huhsway <seunghoe9542@naver.com>
2021-05-03 19:23:05 +0900

    플로이드 알고리즘 시간 복잡도 개선

--- CHAPTER 09 최단 경로/9-4.py
+++ CHAPTER 09 최단 경로/9-4.py
@@ -6,10 +6,12 @@ n, m = map(int,input().split())
 graph = [[INF] * (n+1) for _ in range(n+1)]
 
 # 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
-for a in range(1, n+1):
-    for b in range(1, n+1):
-        if a ==  b:
-            graph[a][b] = 0
+# for a in range(1, n+1):
+#     for b in range(1, n+1):
+#         if a ==  b:
+#             graph[a][b] = 0
+for i in range(1,n+1):
+    graph[i][i] = 0
 
 # 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
 for _ in range(m):
@@ -25,7 +27,9 @@ x, k = map(int,input().split())
 for k in range(1, n+1):
     for a in range(1, n+1):
         for b in range(1, n+1):
-            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+            # graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+            if graph[a][b] > graph[a][k] + graph[k][b]:
+                graph[a][b] = graph[a][k] + graph[k][b]
 
 # 수행된 결과를 출력
 distance = graph[1][k] + graph[k][x]
